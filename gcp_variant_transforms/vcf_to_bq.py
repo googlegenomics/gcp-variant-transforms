5b200f6db093114b823ec115fbee2ae2cc69600a
Bashir Sadjad <bashir.sadjad@gmail.com>
2018-03-02 17:30:54 -0500

    Added an INFO message for the full command.
    Tested: Ran manually and checked the new log message.

--- gcp_variant_transforms/vcf_to_bq.py
+++ gcp_variant_transforms/vcf_to_bq.py
@@ -37,6 +37,7 @@ import argparse
 import datetime
 import enum
 import logging
+import sys
 import tempfile
 
 import apache_beam as beam
@@ -209,6 +210,7 @@ def _validate_args(options, parsed_args):
 
 def run(argv=None):
   """Runs VCF to BigQuery pipeline."""
+  logging.info('Command: %s', ' '.join(argv or sys.argv))
   parser = argparse.ArgumentParser()
   parser.register('type', 'bool', lambda v: v.lower() == 'true')
   command_line_options = [option() for option in _COMMAND_LINE_OPTIONS]
