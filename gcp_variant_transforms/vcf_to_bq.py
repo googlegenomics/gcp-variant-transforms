aa44115ee47f8f3ddf512ea508e0c47cf5ae81e6
Nima Mousavi <4221028+nmousavi@users.noreply.github.com>
2018-04-27 16:39:48 -0400

    Fix Build Fail 490618bd-6fbe-4462-a46d-c80310e15d30
    readvariant module uses rep header for parsing VCF files. Overwriting
    rep header flag before writing value into it, gave a false signal that
    this file exists. Pushed flag overwirte after writing header definitions
    to it.

--- gcp_variant_transforms/vcf_to_bq.py
+++ gcp_variant_transforms/vcf_to_bq.py
@@ -133,7 +133,7 @@ def _merge_headers(known_args, pipeline_args, pipeline_mode):
       datetime.datetime.now().strftime('%Y%m%d-%H%M%S'),
       google_cloud_options.job_name,
       _MERGE_HEADERS_FILE_NAME])
-  known_args.representative_header_file = filesystems.FileSystems.join(
+  temp_merged_headers_file_path = filesystems.FileSystems.join(
       temp_directory, temp_merged_headers_file_name)
 
   with beam.Pipeline(options=options) as p:
@@ -141,9 +141,8 @@ def _merge_headers(known_args, pipeline_args, pipeline_mode):
     merged_header = vcf_to_bq_common.get_merged_headers(headers, known_args)
     if known_args.infer_undefined_headers:
       merged_header = _add_inferred_headers(p, known_args, merged_header)
-    vcf_to_bq_common.write_headers(merged_header,
-                                   known_args.representative_header_file)
-
+    vcf_to_bq_common.write_headers(merged_header, temp_merged_headers_file_path)
+    known_args.representative_header_file = temp_merged_headers_file_path
 
 def run(argv=None):
   # type: (List[str]) -> None
