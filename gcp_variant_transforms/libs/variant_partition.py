f686d2677c309b87af23b0201ec8ffc194700b2a
Saman Vaisipour <svaisipour@gmail.com>
2018-05-01 14:24:30 -0400

    Partitioning Part 2 (input config file)
    Following the PR #195, here we add the functionality that user can input
    a config file to partition output tables to optimise their future BQ
    query costs.

--- gcp_variant_transforms/libs/variant_partition.py
+++ gcp_variant_transforms/libs/variant_partition.py
@@ -22,31 +22,202 @@ partition_for() function used by DataFlow pipeline.
 from __future__ import absolute_import
 
 import re
+from apache_beam.io.filesystems import FileSystems
+from collections import defaultdict
 from mmh3 import hash  # pylint: disable=no-name-in-module,redefined-builtin
-
+import yaml
+import intervaltree
+>>>>>>> Partitioning Part 2 (input config file)
 
 # A reg exp that will match to standard reference_names such as "chr01" or "13".
 _CHROMOSOME_NAME_REGEXP = re.compile(r'^(chr)?([0-9][0-9]?)$')
 # Partition 0 to 21 is reserved for common reference_names such as "chr1".
 _RESERVED_AUTO_PARTITIONS = 22
-# We try to assign each chromosome to a partition. The first 22 partitions
-# [0, 22) are reserved for standard reference_names. Every other identifiers
-# will be matched to next available partitions [22, 27).
+# If config_file_path_given == false we try to assign each chromosome to a partition.
+# The first 22 partitions [0, 22) are reserved for standard reference_names.
+# Every other identifiers will be matched to next available partitions [22, 27).
 _DEFAULT_NUM_PARTITIONS = _RESERVED_AUTO_PARTITIONS + 5
 
+# At most 1000 partitions can be set as output of VariantTransform.
+_MAX_NUM_PARTITIONS = 1000
+# Each partition can contain at most 64 regions.
+_MAX_NUM_REGIONS = 64
+# Matches to regions formatted as 'chr12:10,000-20,000' used in par_config.yaml
+_REGION_LITERAL_REGEXP = re.compile(r'^(\S+):([0-9,]+)-([0-9,]+)$')
+
+
+class RegionIndexer(object):
+  """Assigns indices to multiple regions or one index to a whole chromosome.
+
+  If self._index != -1 then this instance is encoding one whole chromosome.
+  If self._interval_tree != None this intance is encoding multiple regions.
+  """
+  def __init__(self):
+    # Each instance contains either:
+    #    one whole chromosome
+    self._index = -1
+    #    OR multiple regions of one chromosome
+    self._interval_tree = None
+
+  def add_interval(self, start, end, index):
+    if self._index != -1:
+      raise ValueError('You can not add region to a full chromosome instance.')
+    if start < 0:
+      raise ValueError('Start position on a region cannot be negative: {}'.
+                       format(start))
+    if end <= start:
+      raise ValueError('End position must be larger than start position: {} '
+                       'vs {}'.format(end, start))
+    if index < 0:
+      raise ValueError('Index of a region cannot be negative {}'.format(index))
+    if not self._interval_tree:
+      self._interval_tree = intervaltree.IntervalTree()
+    else:
+      if self._interval_tree.overlaps_range(start, end):
+        raise ValueError('Cannot add overlapping region {}-{}'.
+                         format(start, end))
+    # If everything goes well we add the new region to the interval tree.
+    self._interval_tree.addi(start, end, index)
+
+  def add_index(self, index):
+    if self._interval_tree is not None:
+      raise ValueError('You can not add full chromosome to multi region '
+                       'instance.')
+    if index < 0:
+      raise ValueError('Index of a region cannot be negative {}'.format(index))
+    self._index = index
+
+  def get_index(self, pos=0):
+    if self._interval_tree is None and self._index == -1:
+      raise ValueError('This instance has not been assigned any role.')
+    if self._interval_tree is None:
+      return self._index
+    else:
+      matched_intervals = self._interval_tree.search(pos)
+      if len(matched_intervals) > 1:
+        raise ValueError('Search for pos {} returned more than 1 result'.
+                         format(pos))
+      if len(matched_intervals) == 1:
+        return next(iter(matched_intervals)).data
+      else:
+        return -1
+
+  @property
+  def is_full_chromosome(self):
+    return self._index != -1
 
 class VariantPartition(object):
-  """Automatically partition variants based on their reference_name."""
+  """Partition variants based on their reference_name and/or position.
 
-  def __init__(self):
-    if _DEFAULT_NUM_PARTITIONS <= _RESERVED_AUTO_PARTITIONS:
+  This class has 2 operating modes:
+    1) No config file is given (config_file_path == ''):
+        Automatically partition variants based on their reference_name.
+    2) A config file is given:
+        partitions variants based on input config file.
+  """
+
+  def __init__(self, config_file_path=''):
+    if not config_file_path:
+      if _DEFAULT_NUM_PARTITIONS <= _RESERVED_AUTO_PARTITIONS:
+	raise ValueError(
+	    '_DEFAULT_NUM_PARTITIONS must be > _RESERVED_AUTO_PARTITIONS')
+      self._config_file_path_given = False
+      self._reference_name_to_partition_map = {}
+    else:
+      self._config_file_path_given = True
+      self._parse_config(config_file_path)
+
+  def _parse_config(self, config_file_path):
+    """Parses the given partition config file.
+
+    Args:
+      config_file_path: name of the input partition_config file.
+    Raises:
+      A ValueError if any of the expected config formats are violated.
+    """
+
+    def _parse_position(pos_str):
+      return int(pos_str.replace(',', ''))
+
+    with FileSystems.open(config_file_path, 'r') as f:
+      try:
+        partition_configs = yaml.load(f)
+      except yaml.YAMLError as e:
+        raise ValueError('Invalid yaml file: %s' % str(e))
+
+    if len(partition_configs) > _MAX_NUM_PARTITIONS:
       raise ValueError(
-          '_DEFAULT_NUM_PARTITIONS must be > _RESERVED_AUTO_PARTITIONS')
-    self._reference_name_to_partition_map = {}
+          'There can be at most {} partitions but given config file contains {}'
+          .format(_MAX_NUM_PARTITIONS, len(partition_configs)))
+    if len(partition_configs) == 0:
+      raise ValueError('There must be at least one partition in config file.')
+
+    self._partition_no = len(partition_configs)
+
+    self._by_ref_name = defaultdict(RegionIndexer)
+
+    self._suffixes = []
+    table_suffix_duplicate = {}
+
+    # Default partition will contain all remaining readings that do not match to
+    # any other partition. -1 means it does not exist.
+    self._default_partition_index = -1
+    partition_index = 0
+    for partition_config in partition_configs:
+      partition = partition_config.get('partition')
+      regions = partition.get('regions')
+      if regions is None:
+        if self._default_partition_index != -1:
+          raise ValueError(
+              'There must be only one default partition intercepted at least 2')
+        else:
+          self._default_partition_index = partition_index
+      else:
+        if len(regions) > _MAX_NUM_REGIONS:
+          raise ValueError(
+              'At most {} regions per partition, thie partition  contains {}'
+              .format(_MAX_NUM_REGIONS, len(regions)))
+
+        for r in regions:
+          matched = _REGION_LITERAL_REGEXP.match(r)
+          if matched:
+            ref_name, start, end = matched.groups()
+            ref_name = ref_name.strip().lower()
+            start = _parse_position(start)
+            end = _parse_position(end)
+
+            region_indexer = self._by_ref_name.get(ref_name, None)
+            if region_indexer and region_indexer.is_full_chromosome:
+              raise ValueError('A full chromosome must be disjoint from all '
+                               'other regions, {} is not'.format(ref_name))
+            self._by_ref_name[ref_name].add_interval(start, end,
+                                                     partition_index)
+          else:
+            # This region includes a full chromosome
+            ref_name = r.strip().lower()
+            if self._by_ref_name.get(ref_name, None):
+              raise ValueError('A full chromosome must be disjoint from all '
+                               'other regions, {} is not'.format(ref_name))
+            self._by_ref_name[ref_name].add_index(partition_index)
+
+      suffix = partition.get('partition_name').strip()
+      if table_suffix_duplicate.get(suffix, None) is not None:
+        raise ValueError('Table names need to be unique, {} is duplicated'
+                         .format(suffix))
+      table_suffix_duplicate[suffix] = True
+      self._suffixes.append(suffix)
+      partition_index += 1
 
   def get_num_partitions(self):
     """Returns the number of partitions."""
-    return _DEFAULT_NUM_PARTITIONS
+    if not self._config_file_path_given:
+      return _DEFAULT_NUM_PARTITIONS
+    else:
+      if self.is_default_partition_absent():
+        # In this case we have a dummy partition for all remaining variants.
+        return self._partition_no + 1
+      else:
+        return self._partition_no
 
   def get_partition(self, reference_name, pos=0):
     # type: (str, int) -> int
@@ -54,7 +225,20 @@ class VariantPartition(object):
     if not reference_name or pos < 0:
       raise ValueError('Cannot partition given input {}:{}'
                        .format(reference_name, pos))
-    return self._get_auto_partition(reference_name)
+    if self._config_file_path_given:
+      return self._get_config_partition(reference_name, pos)
+    else:
+      return self._get_auto_partition(reference_name)
+
+  def _get_config_partition(self, reference_name, pos):
+    # Lookup _by_ref_name dict to see if we have an entry for this ref_name.
+    indexer = self._by_ref_name.get(reference_name, None)
+    if indexer is not None:
+      index = indexer.get_index(pos)
+      if index != -1:
+        return index
+    # No full/partial chromosome match, return default partition.
+    return self.get_default_partition_index()
 
   def _get_auto_partition(self, reference_name):
     # type: (str) -> int
@@ -67,7 +251,7 @@ class VariantPartition(object):
     If the result of lookup is None, we will try the following steps:
       2) Match the reference_name to a reg exp of common names (eg 'chr12')
       3) Hash the reference_name and calculate its mod to remaining buckets
-    result will be added to _reference_name_to_partition_map for future lookup.
+    result will be added to _reference_name_to_partition_map for future lookups.
 
     Args:
       reference_name: reference name of the variant which is being partitioned
@@ -92,3 +276,26 @@ class VariantPartition(object):
       # Save partition in _reference_name_to_partition dict for future lookups
       self._reference_name_to_partition_map[reference_name] = partition
     return partition
+
+  def is_default_partition_absent(self):
+    if self._config_file_path_given:
+      return self._default_partition_index == -1
+    else:
+      # default partition does not exist, return -1.
+      return False
+
+  def get_default_partition_index(self):
+    # If not _config_file_path_given default partition does not exist, return -1.
+    if not self._config_file_path_given:
+      return -1
+
+    if self.is_default_partition_absent():
+      return self._partition_no
+    else:
+      return self._default_partition_index
+
+  def get_suffix(self, index):
+    if index >= self._partition_no or index < 0:
+      raise ValueError('given partition index is larger than the no of '
+                       'partitions {} vs {}'.format(index, self._partition_no))
+    return self._suffixes[index]
--- gcp_variant_transforms/libs/variant_partition_test.py
+++ gcp_variant_transforms/libs/variant_partition_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for metrics_util module."""
+"""Unit tests for variant_partition module."""
 
 from __future__ import absolute_import
 
@@ -62,3 +62,227 @@ class VariantPartitionTest(unittest.TestCase):
 
     with self.assertRaisesRegexp(ValueError, 'Cannot partition given input*'):
       partitioner.get_partition('  ', 1)
+
+  def test_config_boundaries(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config1.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 8)
+    self.assertEqual(partitioner.get_default_partition_index(), 7)
+    self.assertFalse(partitioner.is_default_partition_absent())
+
+    # "chr1:0-1,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('chr1', 999999), 0)
+    # "chr1:1,000,000-2,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 1000000), 1)
+    self.assertEqual(partitioner.get_partition('chr1', 1999999), 1)
+    # "chr1:2,000,000-999,999,999"
+    self.assertEqual(partitioner.get_partition('chr1', 2000000), 2)
+    self.assertEqual(partitioner.get_partition('chr1', 999999998), 2)
+    self.assertEqual(partitioner.get_partition('chr1', 999999999), 7)
+
+    # "chr2" OR "ch2" OR "c2" OR "2"
+    self.assertEqual(partitioner.get_partition('chr2', 0), 3)
+    self.assertEqual(partitioner.get_partition('chr2', 999999999000), 3)
+    self.assertEqual(partitioner.get_partition('ch2', 0), 3)
+    self.assertEqual(partitioner.get_partition('ch2', 999999999000), 3)
+    self.assertEqual(partitioner.get_partition('c2', 0), 3)
+    self.assertEqual(partitioner.get_partition('c2', 999999999000), 3)
+    self.assertEqual(partitioner.get_partition('2', 0), 3)
+    self.assertEqual(partitioner.get_partition('2', 999999999000), 3)
+
+    # "C4" OR "cr5" OR "chr6:1,000,000-2,000,000"
+    self.assertEqual(partitioner.get_partition('c4', 0), 4)
+    self.assertEqual(partitioner.get_partition('c4', 999999999000), 4)
+    self.assertEqual(partitioner.get_partition('cr5', 0), 4)
+    self.assertEqual(partitioner.get_partition('cr5', 999999999000), 4)
+    self.assertEqual(partitioner.get_partition('chr6', 1000000), 4)
+    self.assertEqual(partitioner.get_partition('chr6', 2000000 - 1), 4)
+    self.assertEqual(partitioner.get_partition('chr6', 0), 7)
+    self.assertEqual(partitioner.get_partition('chr6', 999999), 7)
+    self.assertEqual(partitioner.get_partition('chr6', 2000000), 7)
+
+    # "3:0-500,000"
+    self.assertEqual(partitioner.get_partition('3', 0), 5)
+    self.assertEqual(partitioner.get_partition('3', 499999), 5)
+    # "3:500,000-1,000,000"
+    self.assertEqual(partitioner.get_partition('3', 500000), 6)
+    self.assertEqual(partitioner.get_partition('3', 999999), 6)
+    self.assertEqual(partitioner.get_partition('3', 1000000), 7)
+
+  def test_config_case_insensitive(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config1.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 8)
+    self.assertEqual(partitioner.get_default_partition_index(), 7)
+    self.assertFalse(partitioner.is_default_partition_absent())
+
+    # "chr1:0-1,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('Chr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('cHr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('chR1', 0), 0)
+    self.assertEqual(partitioner.get_partition('CHr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('ChR1', 0), 0)
+    self.assertEqual(partitioner.get_partition('cHR1', 0), 0)
+    self.assertEqual(partitioner.get_partition('CHR1', 0), 0)
+
+  def test_config_get_suffix(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config1.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 8)
+    self.assertEqual(partitioner.get_default_partition_index(), 7)
+    self.assertFalse(partitioner.is_default_partition_absent())
+
+    self.assertEqual(partitioner.get_suffix(0), 'chr01_part1')
+    self.assertEqual(partitioner.get_suffix(1), 'chr01_part2')
+    self.assertEqual(partitioner.get_suffix(2), 'chr01_part3')
+    self.assertEqual(partitioner.get_suffix(3), 'chrom02')
+    self.assertEqual(partitioner.get_suffix(4), 'chrom04_05_and_part_06')
+    self.assertEqual(partitioner.get_suffix(5), 'chr3_01')
+    self.assertEqual(partitioner.get_suffix(6), 'chr3_02')
+    self.assertEqual(partitioner.get_suffix(7), 'all_remaining')
+
+
+  def test_config_non_existent_suffix(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config1.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 8)
+
+    with self.assertRaises(ValueError):
+      partitioner.get_suffix(-1)
+      self.fail('Non existent partition index should throw an exception')
+    with self.assertRaises(ValueError):
+      partitioner.get_suffix(8)
+      self.fail('Non existent partition index should throw an exception')
+
+  def test_config_default_partition_in_middle(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config2.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 5)
+    self.assertEqual(partitioner.get_default_partition_index(), 1)
+    self.assertFalse(partitioner.is_default_partition_absent())
+
+    # "chr1:0-1,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('chr1', 999999), 0)
+    # "chr1:1,000,000-2,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 1000000), 2)
+    self.assertEqual(partitioner.get_partition('chr1', 1999999), 2)
+    # "chr2" OR "ch2" OR "c2" OR "2"
+    self.assertEqual(partitioner.get_partition('chr2', 0), 3)
+    self.assertEqual(partitioner.get_partition('chr2', 999999999000), 3)
+    # "3:500,000-1,000,000"
+    self.assertEqual(partitioner.get_partition('3', 500000), 4)
+    self.assertEqual(partitioner.get_partition('3', 999999), 4)
+
+    # All the followings are assigned to default partition.
+    self.assertEqual(partitioner.get_partition('chr1', 2000000),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('chr1', 999999999),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('3', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('3', 499999),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('3', 1000000),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('ch2', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('c2', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('2', 0),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('c4', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('cr5', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('chr6', 0),
+                     partitioner.get_default_partition_index())
+
+  def test_config_default_partition_absent(self):
+    partitioner = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/partition_config3.yaml")
+    self.assertEqual(partitioner.get_num_partitions(), 5)
+    self.assertEqual(partitioner.get_default_partition_index(), 4)
+    self.assertTrue(partitioner.is_default_partition_absent())
+
+    # "chr1:0-1,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 0), 0)
+    self.assertEqual(partitioner.get_partition('chr1', 999999), 0)
+    # "chr1:1,000,000-2,000,000"
+    self.assertEqual(partitioner.get_partition('chr1', 1000000), 1)
+    self.assertEqual(partitioner.get_partition('chr1', 1999999), 1)
+    # "chr2" OR "ch2" OR "c2" OR "2"
+    self.assertEqual(partitioner.get_partition('chr2', 0), 2)
+    self.assertEqual(partitioner.get_partition('chr2', 999999999000), 2)
+    # "3:500,000-1,000,000"
+    self.assertEqual(partitioner.get_partition('3', 500000), 3)
+    self.assertEqual(partitioner.get_partition('3', 999999), 3)
+
+    # All the followings are assigned to default partition.
+    self.assertEqual(partitioner.get_partition('chr1', 2000000),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('chr1', 999999999),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('3', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('3', 499999),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('3', 1000000),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('ch2', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('c2', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('2', 0),
+                     partitioner.get_default_partition_index())
+
+    self.assertEqual(partitioner.get_partition('c4', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('cr5', 0),
+                     partitioner.get_default_partition_index())
+    self.assertEqual(partitioner.get_partition('chr6', 0),
+                     partitioner.get_default_partition_index())
+
+  def test_config_failed_config_validation(self):
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+          "gcp_variant_transforms/testing/data/misc/"
+          "partition_config_redundant_default.yaml")
+      self.fail('Broken config file should throw an exception')
+
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+          "gcp_variant_transforms/testing/data/misc/"
+          "partition_config_overlapping_regions.yaml")
+      self.fail('Broken config file should throw an exception')
+
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+          "gcp_variant_transforms/testing/data/misc/"
+          "partition_config_full_and_partial_chr.yaml")
+      self.fail('Broken config file should throw an exception')
+
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+          "gcp_variant_transforms/testing/data/misc/"
+          "partition_config_redundant_full_chr.yaml")
+      self.fail('Broken config file should throw an exception')
+
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+        "gcp_variant_transforms/testing/data/misc/"
+        "partition_config_conflicting_full_chr.yaml")
+      self.fail('Broken config file should throw an exception')
+
+    with self.assertRaises(ValueError):
+      _ = variant_partition.VariantPartition(
+          "gcp_variant_transforms/testing/data/misc/"
+          "partition_config_redundant_table_names.yaml")
+      self.fail('Broken config file should throw an exception')
