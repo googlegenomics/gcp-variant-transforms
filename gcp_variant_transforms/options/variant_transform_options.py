d8a766d1fa0898fb5227ccfacccf6890eeda59c8
yifangchen <yifangchen@google.com>
2018-05-08 09:52:31 -0400

    test pipeline mode

--- gcp_variant_transforms/options/variant_transform_options.py
+++ gcp_variant_transforms/options/variant_transform_options.py
@@ -368,3 +368,9 @@ class PreprocessOptions(VariantTransformsOptions):
         help=('The full path of the resolved headers. The file will not be'
               'generated if unspecified. Otherwise, please provide a local '
               'path if run locally, or a cloud path if run on Dataflow.'))
+    parser.add_argument(
+        '--optimize_for_large_inputs',
+        type='bool', default=False, nargs='?', const=True,
+        help=('If true, the pipeline runs in optimized way for handling large '
+              'inputs. Set this to true if you are loading more than 50,000 '
+              'files.'))
