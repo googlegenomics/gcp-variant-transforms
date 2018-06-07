54b27a9e7d285a1753a7aa6b9102c6f5cb17c407
allieychen <36679499+allieychen@users.noreply.github.com>
2018-06-07 12:49:19 -0400

    Add more tests for preprocessor #206 (#256)
    Add more tests for preprocessor pipeline.
    
    Issue: issue 206

--- gcp_variant_transforms/libs/preprocess_reporter.py
+++ gcp_variant_transforms/libs/preprocess_reporter.py
@@ -166,7 +166,7 @@ def _append_inferred_headers_to_report(file_to_write, inferred_headers):
 
 def _append_malformed_records_to_report(file_to_write, malformed_records):
   # type: (file, Optional[List[MalformedVcfRecord]]) -> None
-  """Appends the human readable malformed records to the report.
+  """Appends the human readable malformed records (sorted) to the report.
 
   Output example:
   Malformed Records
@@ -175,9 +175,9 @@ def _append_malformed_records_to_report(file_to_write, malformed_records):
   """
   if malformed_records is not None:
     content_lines = []
-    for record in malformed_records:
+    for record in sorted(malformed_records):
       content_lines.append(_DELIMITER.join([record.file_name,
-                                            record.line,
+                                            record.line.replace('\t', ' '),
                                             record.error]))
     _append_to_report(file_to_write, _InconsistencyType.MALFORMED_RECORDS,
                       _HeaderLine.MALFORMED_RECORDS_HEADER, content_lines)
@@ -189,7 +189,7 @@ def _generate_conflicting_headers_lines(
     category  # type: str
     ):
   # type: (...) -> List[str]
-  """Returns the conflicting headers lines for the report.
+  """Returns the conflicting headers lines (sorted) for the report.
 
   Each conflicting header record is structured into columns(TAB separated
   values): the ID, the category('FOMRAT' or 'INFO'), conflicting definitions,
@@ -204,24 +204,25 @@ def _generate_conflicting_headers_lines(
                     num=1 type=Integer   file2
   """
   content_lines = []
-  for field_id, definitions_to_files_map in conflicts.iteritems():
+  for field_id in sorted(conflicts.keys()):
     first_item = True
-    for definition, file_names in definitions_to_files_map.iteritems():
+    for definition in sorted(conflicts.get(field_id).keys()):
+      sorted_file_names = sorted(conflicts.get(field_id).get(definition))
       if first_item:
         row = [field_id,
                category,
                _format_definition(definition.num, definition.type),
-               file_names[0],
+               sorted_file_names[0],
                _extract_resolution(resolved_headers, field_id)]
         first_item = False
       else:
         row = [_PADDING_CHARACTER,
                _PADDING_CHARACTER,
                _format_definition(definition.num, definition.type),
-               file_names[0],
+               sorted_file_names[0],
                _PADDING_CHARACTER]
       content_lines.append(_DELIMITER.join(row))
-      for file_name in file_names[1:]:
+      for file_name in sorted_file_names[1:]:
         row = [_PADDING_CHARACTER,
                _PADDING_CHARACTER,
                _PADDING_CHARACTER,
@@ -233,7 +234,7 @@ def _generate_conflicting_headers_lines(
 
 def _generate_inferred_headers_lines(inferred_headers, category):
   # type: (Dict[str, Dict[str, Union[str, int]]], str) -> List[str]
-  """Returns the inferred headers lines for the report.
+  """Returns the inferred headers lines (sorted) for the report.
 
   The field ID, category (FORMAT or INFO), and the inferred header definitions
   are included in the contents.
@@ -241,7 +242,7 @@ def _generate_inferred_headers_lines(inferred_headers, category):
   GT    INFO    num=1 type=Float
   """
   content_lines = []
-  for field_id in inferred_headers.keys():
+  for field_id in sorted(inferred_headers.keys()):
     row = [field_id,
            category,
            _extract_resolution(inferred_headers, field_id)]
--- gcp_variant_transforms/testing/integration/run_preprocessor_tests.py
+++ gcp_variant_transforms/testing/integration/run_preprocessor_tests.py
@@ -57,7 +57,8 @@ class PreprocessorTestCase(object):
                test_name,  # type: str
                input_pattern,  # type: str
                expected_contents,  # type: List[str]
-               blob_name,  # type: str
+               report_blob_name,  # type: str
+               header_blob_name=None,  # type: str
                **kwargs  # type: **str
               ):
     # type: (...) -> None
@@ -66,8 +67,8 @@ class PreprocessorTestCase(object):
     self._expected_contents = expected_contents
     suffix = '_integration_tests_{}'.format(
         datetime.now().strftime('%Y%m%d_%H%M%S'))
-    self._blob_name = ''.join([blob_name, suffix])
-    self._report_path = '/'.join(['gs:/', _BUCKET_NAME, self._blob_name])
+    self._report_blob_name = self._append_suffix(report_blob_name, suffix)
+    self._report_path = '/'.join(['gs:/', _BUCKET_NAME, self._report_blob_name])
     self._project = parser_args.project
     args = ['--input_pattern {}'.format(input_pattern),
             '--report_path {}'.format(self._report_path),
@@ -76,6 +77,14 @@ class PreprocessorTestCase(object):
             '--temp_location {}'.format(parser_args.temp_location),
             '--job_name {}'.format(
                 ''.join([test_name, suffix]).replace('_', '-'))]
+    self._header_blob_name = None
+    self._header_path = None
+    if header_blob_name:
+      self._header_blob_name = self._append_suffix(header_blob_name, suffix)
+      self._header_path = '/'.join(['gs:/',
+                                    _BUCKET_NAME,
+                                    self._header_blob_name])
+      args.append('--resolved_headers_path {}'.format(self._header_path))
     for k, v in kwargs.iteritems():
       args.append('--{} {}'.format(k, v))
 
@@ -88,24 +97,40 @@ class PreprocessorTestCase(object):
 
     - Checks that the report is generated.
     - Validates report's contents are the same as `expected_contents`.
+    - Checks that the resolved headers are generated if `header_blob_name` is
+      specified in the test.
     """
     client = storage.Client(self._project)
     bucket = client.get_bucket(_BUCKET_NAME)
-    blob = bucket.get_blob(self._blob_name)
-    if not blob.exists(client):
+    report_blob = bucket.get_blob(self._report_blob_name)
+    if not report_blob:
       raise run_tests_common.TestCaseFailure(
           'Report is not generated in {}'.format(self._report_path))
-    contents = blob.download_as_string()
-    expected_contents = ''.join(self._expected_contents)
+    contents = report_blob.download_as_string()
+    expected_contents = '\n'.join(self._expected_contents)
     if expected_contents != contents:
       raise run_tests_common.TestCaseFailure(
           'Contents mismatch: expected {}, got {}'.format(
               expected_contents, contents))
     if not self._keep_reports:
-      blob.delete()
+      report_blob.delete()
+
+    if self._header_blob_name:
+      resolved_headers_blob = bucket.get_blob(self._header_blob_name)
+      if not resolved_headers_blob:
+        raise run_tests_common.TestCaseFailure(
+            'The resolved header is not generated in {}'.format(
+                self._header_path))
+      if not self._keep_reports:
+        resolved_headers_blob.delete()
 
   def get_name(self):
     return self._name
+
+  def _append_suffix(self, file_path, suffix):
+    # type: (str, str) -> str
+    file_name, file_extension = os.path.splitext(file_path)
+    return ''.join([file_name, suffix, file_extension])
 
 
 def _get_args():
@@ -113,14 +138,15 @@ def _get_args():
   run_tests_common.add_args(parser)
   parser.add_argument('--keep_reports',
                       type=bool, default=False, nargs='?', const=True,
-                      help='If set, generated reports are not deleted.')
+                      help=('If set, generated reports and resolved headers '
+                            'are not deleted.'))
   return parser.parse_args()
 
 
 def _get_test_configs():
   # type: () -> List[Dict]
   """Gets all test configs in preprocessor_tests."""
-  required_keys = ['test_name', 'blob_name', 'input_pattern',
+  required_keys = ['test_name', 'report_blob_name', 'input_pattern',
                    'expected_contents']
   test_file_path = os.path.join(os.getcwd(), _TEST_FOLDER)
   return run_tests_common.get_configs(test_file_path, required_keys)
