8d372fd80c11d33369699f5367c24c88e333540e
Jessime <jessime.kirk@gmail.com>
2018-07-03 15:32:58 -0400

    addressed all comments from PR review.

--- gcp_variant_transforms/libs/processed_variant_test.py
+++ gcp_variant_transforms/libs/processed_variant_test.py
@@ -468,25 +468,23 @@ class ProcessedVariantFactoryTest(unittest.TestCase):
 
   def test_create_alt_bases_field_schema_descriptions(self):
     _, header_fields = self._get_sample_variant_and_header_with_csq()
-    # return header_fields
     for hfi in header_fields.infos.values():
-        hfi['type'] = 'string'
+      hfi['type'] = 'string'
 
     counter_factory = _CounterSpyFactory()
     factory = processed_variant.ProcessedVariantFactory(
         header_fields,
         split_alternate_allele_info_fields=True,
         annotation_fields=['CSQ'],
-        minimal_match=True,
         counter_factory=counter_factory)
     schema = factory.create_alt_bases_field_schema()
-    csq_field = schema.fields[2]
-    name_desc_map  = {'Consequence': 'consequence type of this variant',
-                       'IMPACT': 'the impact modifier for the consequence type',
-                       'SYMBOL': 'the gene symbol',
-                       'Gene': 'Ensembl stable ID of affected gene'}
+    csq_field = [field for field in schema.fields if field.name == 'CSQ'][0]
+    name_desc_map = {'Consequence': 'Consequence type of this variant',
+                     'IMPACT': 'The impact modifier for the consequence type',
+                     'SYMBOL': 'The gene symbol',
+                     'Gene': 'Ensembl stable ID of affected gene',
+                     'allele': 'The ALT part of the annotation field.'}
     for field in csq_field.fields:
-        if field.name in name_desc_map:
-            self.assertEqual(field.description, name_desc_map[field.name])
-
-# TODO(bashir2): Add tests for create_alt_record_for_schema.
+      self.assertEqual(field.description, name_desc_map[field.name])
+    alt_field = [field for field in schema.fields if field.name == 'alt'][0]
+    self.assertEqual(alt_field.description, 'Alternate base.')
