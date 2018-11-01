d40bfd3d7afacb86a88dcb9dd5afbb40f3c44a43
Allie Chen <36679499+allieychen@users.noreply.github.com>
2018-11-01 13:03:30 -0400

    small fixes (#407)
    - Use test name in integration tests error message instead of the operation name to be more informative.
    - When the data is small we always run pipeline locally, unless --infer_headers is provided. Add --infer_annotation_types as well since it also needs to read the variants. (The valid_4_2_VEP.json test case now runs correctly when using BundleBasedDirectRunner with a higher version of apache beam).
    Related issues: #291

--- gcp_variant_transforms/testing/integration/run_tests_common.py
+++ gcp_variant_transforms/testing/integration/run_tests_common.py
@@ -70,6 +70,7 @@ class TestRunner(object):
         credentials=GoogleCredentials.get_application_default())
     self._revalidate = revalidate
     self._operation_names_to_test_states = {}  # type: Dict[str, TestCaseState]
+    self._operation_names_to_test_case_names = {}  # type: Dict[str, str]
 
   def run(self):
     """Runs all tests."""
@@ -101,6 +102,8 @@ class TestRunner(object):
     operation_name = request.execute()['name']
     self._operation_names_to_test_states.update(
         {operation_name: TestCaseState(test_cases[0], test_cases[1:])})
+    self._operation_names_to_test_case_names.update(
+        {operation_name: test_cases[0]._name})
 
   def _wait_for_all_operations_done(self):
     """Waits until all operations are done."""
@@ -125,13 +128,17 @@ class TestRunner(object):
     if 'error' in response:
       if 'message' in response['error']:
         raise TestCaseFailure(
-            'Operation {} failed: {}'.format(operation_name,
-                                             response['error']['message']))
+            'Test case {} (Operation {}) failed: {}'.format(
+                self._operation_names_to_test_case_names[operation_name],
+                operation_name,
+                response['error']['message']))
       else:
         # This case should never happen.
         raise TestCaseFailure(
-            'Operation {} failed: No traceback. '
-            'See logs for more information on error.'.format(operation_name))
+            'Test case {} (Operation {}) failed: No traceback. '
+            'See logs for more information on error.'.format(
+                self._operation_names_to_test_case_names[operation_name],
+                operation_name))
 
   def print_results(self):
     """Prints results of test cases."""
