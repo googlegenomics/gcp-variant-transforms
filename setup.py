6f089ab94cf8d428e8b9b935955e461c96907125
Allie Chen <36679499+allieychen@users.noreply.github.com>
2018-11-27 14:40:26 -0500

    update the version (#424)
    Update the version for the new release.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class BuildPyCommand(build_py):
 
 setuptools.setup(
     name='gcp_variant_transforms',
-    version='0.5.0',
+    version='0.5.1',
     description=('Tool for transforming and processing VCF files in a '
                  'scalable manner based on Apache Beam'),
     author='Google',
