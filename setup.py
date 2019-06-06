1035fed559e1c0dfbd3d92f8c0846bfd74c4aa43
Allie Chen <36679499+allieychen@users.noreply.github.com>
2019-06-06 12:07:55 -0400

    Beam has an update and changes the dependencies on the version of google-cloud-core. (#488)

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ REQUIRED_PACKAGES = [
     # Nucleus needs uptodate protocol buffer compiler (protoc).
     'protobuf>=3.6.1',
     'mmh3<2.6',
-    # Need to explicitly install v<=1.13.0. apache-beam requires
-    # google-cloud-pubsub 0.39.0, which relies on google-cloud-core<0.30dev,
-    # >=0.29.0. google-cloud-storage also has requirements on google-cloud-core,
-    # and version 1.13.0 resolves the dependency conflicts.
-    'google-cloud-storage<=1.13.0'
+    # Need to explicitly install v<=1.14.0. apache-beam requires
+    # google-cloud-pubsub 0.39.1, which relies on google-cloud-core<0.30dev,
+    # >=0.29.1. google-cloud-storage also has requirements on google-cloud-core,
+    # and version 1.14.0 resolves the dependency conflicts.
+    'google-cloud-storage<=1.14.0'
 ]
 
 REQUIRED_SETUP_PACKAGES = [
