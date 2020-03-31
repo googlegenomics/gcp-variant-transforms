c0ad470bf50095b2f36e19936367a67152d77b97
Saman Vaisipour <svaisipour@gmail.com>
2020-03-31 18:22:00 -0400

    Add a method to create composite sample name (#572)
    * Add a method to create composite sample name
    
    based on VCF file_path and sample_name inside the VCF file.
    
    This change will make our sample_info_table to have one-to-one relation between sample_name and sample_id. This will make the logic of BQ_to_VCF more straightforward, both for VT users and VT developers.
    
    * Addressing comments

--- gcp_variant_transforms/libs/hashing_util_test.py
+++ gcp_variant_transforms/libs/hashing_util_test.py
@@ -38,17 +38,18 @@ class HashingUtilTest(unittest.TestCase):
     self.assertEqual(hash_code, 110)
 
   def test_generate_sample_id_with_file_path(self):
-    hash_code = hashing_util.generate_sample_id('Sample1', 'file_1')
-    self.assertEqual(hash_code, 1603149767211015963)
-
-    hash_code = hashing_util.generate_sample_id('Sample2', 'file_1')
-    self.assertEqual(hash_code, 7039455832764509387)
-
-    hash_code = hashing_util.generate_sample_id('Sample1', 'file_2')
-    self.assertEqual(hash_code, 4840534050208649594)
-
-    hash_code = hashing_util.generate_sample_id('Sample2', 'file_2')
-    self.assertEqual(hash_code, 7113221774487715893)
+    hash_code = hashing_util.generate_sample_id('Sample1',
+                                                'gs://bucket1/dir1/file1.vcf')
+    self.assertEqual(hash_code, 7715696391291253656)
+    hash_code = hashing_util.generate_sample_id('Sample2',
+                                                'gs://bucket1/dir1/file1.vcf')
+    self.assertEqual(hash_code, 5682150464643626236)
+    hash_code = hashing_util.generate_sample_id('Sample1',
+                                                'gs://bucket1/dir1/file2.vcf')
+    self.assertEqual(hash_code, 668336000922978678)
+    hash_code = hashing_util.generate_sample_id('Sample2',
+                                                'gs://bucket1/dir1/file2.vcf')
+    self.assertEqual(hash_code, 5498327443813165683)
 
   def test_generate_sample_id_without_file_path(self):
     hash_code = hashing_util.generate_sample_id('Sample1')
@@ -56,3 +57,21 @@ class HashingUtilTest(unittest.TestCase):
 
     hash_code = hashing_util.generate_sample_id('Sample2')
     self.assertEqual(hash_code, 8341768597576477893)
+
+  def test_create_composite_sample_name(self):
+    composite_names = {
+        'gs___bucket1_dir1_file1_vcf_sample1':
+            ('sample1', 'gs://bucket1/dir1/file1.vcf'),
+        'gs___bucket1_dir1_file1_vcf_gz_sample1':
+            ('sample1', 'gs://bucket1/dir1/file1.vcf.gz'),
+        'gs___BUCKET1_DIR1_FILE1_vcf_sample1':
+            ('sample1', 'gs://BUCKET1/DIR1/FILE1.vcf'),
+        'gs___bucket_1_dir_1_file_1_vcf_sample1':
+            ('sample1', 'gs://bucket-1/dir-1/file-1.vcf'),
+        'gs___bucket1_dir1_file1_vcf_sample-@~!*&1':
+            ('sample-@~!*&1', 'gs://bucket1/dir1/file1.vcf'),
+    }
+    for expected_name, inputs in composite_names.items():
+      self.assertEqual(expected_name,
+                       hashing_util.create_composite_sample_name(inputs[0],
+                                                                 inputs[1]))
