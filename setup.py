a16fc2a575dffc4ddd8f2269cf190a8c9511dea3
Tural Neymanov <tneymanov@google.com>
2020-04-20 02:39:59 -0400

    Address first iteration of comments.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ PYSAM_INSTALLATION_COMMAND = ['pip', 'install', 'pysam>=0.15.3']
 
 REQUIRED_PACKAGES = [
     'cython>=0.28.1',
-    'apache-beam[gcp]<=2.20.0',
+    'apache-beam[gcp]==2.20.0',
     # Note that adding 'google-api-python-client>=1.6' causes some dependency
     # mismatch issues. This is fatal if using 'setup.py install', but works on
     # 'pip install .' as it ignores conflicting versions. See Issue #71.
@@ -39,7 +39,6 @@ REQUIRED_PACKAGES = [
     'intervaltree>=2.1.0,<2.2.0',
     'mmh3<2.6',
     'google-cloud-storage',
-    'google-resumable-media<0.6dev,>=0.5.0',
     'pyfarmhash',
     'pyyaml'
 ]
