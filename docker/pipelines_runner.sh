527e1d0688fdab4502a2bcdd4b0aa9309e6d5531
Saman Vaisipour <svaisipour@gmail.com>
2020-06-29 11:45:30 -0400

    Modify how we send subnetwork to pipelines-tools (#627)
    It seems pipelines-tools only need subnetwork value and it generates the correct format itself:
    
    `projects/${google_cloud_project}/regions/${region}/subnetworks/${subnetwork}`

--- docker/pipelines_runner.sh
+++ docker/pipelines_runner.sh
@@ -104,7 +104,7 @@ function main {
 
   if [[ ! -z "${subnetwork}" ]]; then
     echo "Adding --subnetwork ${subnetwork} to optional_args"
-    pt_optional_args="${pt_optional_args} --subnetwork projects/${google_cloud_project}/regions/${region}/subnetworks/${subnetwork}"
+    pt_optional_args="${pt_optional_args} --subnetwork ${subnetwork}"
     df_optional_args="${df_optional_args} --subnetwork https://www.googleapis.com/compute/v1/projects/${google_cloud_project}/regions/${region}/subnetworks/${subnetwork}"
   fi
 
