6f10e30c6ac2983db6669c6600defcea3c2e8a00
Lawrence, Andrew <aelawrence@google.com>
2021-12-22 13:58:34 -0500

    fix formatting

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ import setuptools
 
 
 class build(_build):  # pylint: disable=invalid-name
-    """A build command class that will be invoked during package install.
+  """A build command class that will be invoked during package install.
 
-    The package built using the current setup.py will be staged and later
-    installed in the worker using `pip install package'. This class will be
-    instantiated during install for this specific scenario and will trigger
-    running the custom commands specified.
-    """
+  The package built using the current setup.py will be staged and later
+  installed in the worker using `pip install package'. This class will be
+  instantiated during install for this specific scenario and will trigger
+  running the custom commands specified.
+  """
 
 
 setuptools.setup(
