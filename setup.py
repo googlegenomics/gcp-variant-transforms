47844f7f1d2d40948674d127037eb666ae552beb
Andy Lawrence <andy.lawrence@gmail.com>
2022-03-31 14:31:06 -0400

    release 0.11.0 (#716)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class build(_build):  # pylint: disable=invalid-name
 
 setuptools.setup(
     name='gcp_variant_transforms',
-    version='0.10.0',
+    version='0.11.0',
     description=('Tool for transforming and processing VCF files in a '
                  'scalable manner based on Apache Beam'),
     author='Google',
