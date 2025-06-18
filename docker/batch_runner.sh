452ce77d0f140db6fadc577ea9ee0eb1fd540567
Sieu Huynh <sieu@dataxight.com>
2025-06-18 17:28:14 +0700

    Added dockerfile and script to ingest data using batch

---
+++ docker/batch_runner.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+set -euo pipefail
+
+# Copyright 2019 Google Inc.  All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#################################################
+# Parses arguments and does some sanity checking.
+# Arguments:
+#   It is expected that this is called with $@ of the main script.
+#################################################
+function parse_args {
+  # getopt command is only for checking arguments.
+  getopt -o '' -l project:,temp_location:,docker_image:,sdk_container_image:,region:,subnetwork:,use_public_ips:,service_account:,location:,service_account_credentials: -- "$@"
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      --project)
+        google_cloud_project="$2"
+        ;;
+
+      --region)
+        region="$2"
+        ;;
+
+      --location)
+        location="$2"
+        ;;
+
+      --temp_location)
+        temp_location="$2"
+        ;;
+
+      --docker_image)
+        vt_docker_image="$2"
+        ;;
+
+      --sdk_container_image)
+        sdk_container_image="$2"
+        ;;
+
+      --subnetwork)
+        subnetwork="$2"
+        ;;
+
+      --use_public_ips)
+        use_public_ips="$2"
+        ;;
+
+      --service_account)
+        service_account="$2"
+        ;;
+
+      --service_account_credentials)
+        service_account_credentials="$2"
+        ;;
+
+      *)
+        command="$@"
+        break
+        ;;
+    esac
+    shift 2
+  done
+}
+
+function main {
+  if [[ $1 == /opt/gcp_variant_transforms/bin/* ]]; then
+    exec $@
+  fi
+  parse_args "$@"
+
+  # If missing, we will try to find the default values.
+  google_cloud_project="${google_cloud_project:-$(gcloud config get-value project)}"
+  region="${region:-$(gcloud config get-value compute/region)}"
+  vt_docker_image="${vt_docker_image:-us-east1-docker.pkg.dev/variant-transform-dxt/dxt-public-variant-transform/vt_vep:sieu_test_batch}"
+
+  sdk_container_image="${sdk_container_image:-}"
+  location="${location:-}"
+  temp_location="${temp_location:-}"
+  subnetwork="${subnetwork:-}"
+  use_public_ips="${use_public_ips:-}"
+  service_account="${service_account:-}"
+  service_account_credentials="${service_account_credentials:-}"
+
+  if [[ -z "${google_cloud_project}" ]]; then
+    echo "Please set the google cloud project using flag --project PROJECT."
+    echo "Or set default project in your local client configuration using gcloud config set project PROJECT."
+    exit 1
+  fi
+
+  if [[ -z "${region}" ]]; then
+    echo "Please set the region using flags --region."
+    echo "Or set default region in your local client configuration using gcloud config set compute/region REGION."
+    exit 1
+  fi
+
+  if [[ -z "${temp_location}" ]]; then
+    echo "Please set the temp_location using flag --temp_location."
+    exit 1
+  fi
+
+  if [[ ! -v command ]]; then
+    echo "Please specify a command to run Variant Transforms."
+    exit 1
+  fi
+
+  # Build Dataflow required args based on `docker run ...` inputs.
+  df_required_args="--project ${google_cloud_project} --region ${region} --temp_location ${temp_location}"
+
+  # Build up optional args for pipelines-tools and Dataflow, if they are provided.
+  pt_optional_args=""
+  df_optional_args=""
+
+  if [[ ! -z "${sdk_container_image}" ]]; then
+    echo "Adding --sdk_container_image ${sdk_container_image} to optional_args"
+    df_optional_args="${df_optional_args} --experiments=use_runner_v2 --sdk_container_image=${sdk_container_image}"
+  fi
+
+  if [[ ! -z "${subnetwork}" ]]; then
+    echo "Adding --subnetwork ${subnetwork} to optional_args"
+    pt_optional_args="${pt_optional_args} --subnetwork projects/${google_cloud_project}/regions/${region}/subnetworks/${subnetwork}"
+    df_optional_args="${df_optional_args} --subnetwork https://www.googleapis.com/compute/v1/projects/${google_cloud_project}/regions/${region}/subnetworks/${subnetwork}"
+  fi
+
+  if [[ ! -z "${use_public_ips}" && "${use_public_ips}" == "false" ]]; then
+    echo "Adding --private-address and --no_use_public_ips to optional_args"
+    pt_optional_args="${pt_optional_args} --private-address"
+    df_optional_args="${df_optional_args} --no_use_public_ips"
+  fi
+
+  if [[ ! -z "${service_account}" ]]; then
+    echo "Adding --service-account ${service_account} to optional_args"
+    pt_optional_args="${pt_optional_args} --service-account ${service_account}"
+    df_optional_args="${df_optional_args} --service_account_email ${service_account}"
+  fi
+
+  # Optional location for Life Sciences API (default us-central1), see currently available
+  # locations here: https://cloud.google.com/life-sciences/docs/concepts/locations
+  l_s_location=""
+  if [[ ! -z "${location}" ]]; then
+    echo "Adding --location ${location} to Life Sciences API invocation command."
+    l_s_location="--location ${location}"
+  fi
+
+  export COMMAND="/opt/gcp_variant_transforms/bin/${command}"
+  export DF_OPTIONAL_ARGS="${df_optional_args}"
+  export DF_REQUIRED_ARGS="${df_required_args}"
+  export TEMP_LOCATION="${temp_location}"
+  export LOG_TIME=$(date +%Y%m%d_%H%M%S)
+  export SERVICE_ACCOUNT_EMAIL="${service_account}"
+  export GOOGLE_APPLICATION_CREDENTIALS="/root/.config/gcloud/application_default_credentials.json"
+
+  # Activate the service account if GOOGLE_APPLICATION_CREDENTIALS is set.
+  # if [ ! -f "${SERVICE_ACCOUNT_EMAIL}" ]; then
+  #   echo "Not found: ${SERVICE_ACCOUNT_EMAIL}. Please set it to the path of your service account key file."
+  #   echo "Use default credentials"
+  # else
+  #   export GOOGLE_APPLICATION_CREDENTIALS="${SERVICE_ACCOUNT_EMAIL}"
+  #   gcloud auth activate-service-account --key-file="${SERVICE_ACCOUNT_CREDENTIALS}"
+  # fi
+
+  # Authenticate with the Google Cloud project.
+  # gcloud auth application-default login
+
+  # Create the batch.json file using envsubst to replace variables.
+  envsubst < /opt/gcp_variant_transforms/src/docker/batch.json > batch.json
+
+  # GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" \
+  gcloud batch jobs submit test-batch-job-`date +"%Y%m%d%H%M%S"` \
+    --config=batch.json \
+    --location="${region}"
+}
+
+main "$@"
